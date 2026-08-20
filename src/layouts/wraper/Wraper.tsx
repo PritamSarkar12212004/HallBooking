c7ad12b53d45ad2e75877e0f7470b10a5f0dd53d
Pritam Sarkar <pritamsarkar21122004@gmail.com>
2026-08-20 13:03:41 +0530

    before android file changes

---
+++ src/layouts/wraper/Wraper.tsx
@@ -0,0 +1,55 @@
+import React, { PropsWithChildren } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'react-native';
+
+interface WrapperProps extends PropsWithChildren {
+    style?: StyleProp<ViewStyle>;
+    backgroundColor?: string;
+    safeTop?: boolean;
+    safeBottom?: boolean;
+    safeHorizontal?: boolean;
+    paddingHorizontal?: number;
+    paddingTop?: number;
+    paddingBottom?: number;
+}
+
+const Wrapper = ({
+    children,
+    style,
+    backgroundColor = '#F8FAFC',
+    safeTop = true,
+    safeBottom = false,
+    safeHorizontal = false,
+    paddingHorizontal = 16,
+    paddingTop = 0,
+    paddingBottom = 0,
+}: WrapperProps) => {
+    const insets = useSafeAreaInsets();
+
+    return (
+        <View
+            style={[
+                {
+                    flex: 1,
+                    backgroundColor,
+
+                    paddingTop: safeTop ? insets.top + paddingTop : paddingTop,
+
+                    paddingBottom: safeBottom
+                        ? insets.bottom + paddingBottom
+                        : paddingBottom,
+
+                    paddingHorizontal: safeHorizontal
+                        ? Math.max(insets.left, insets.right) + paddingHorizontal
+                        : paddingHorizontal,
+                },
+                style,
+            ]}
+        >
+            {children}
+        </View>
+    );
+};
+
+export default Wrapper;
